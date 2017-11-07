3d5abb6d1c3a17614234afbb4e75eb4cd0655a04
fgfg163 <fgfg163>
2017-11-08 02:35:58 +0800

    create project

--- index.js
+++ index.js
@@ -1,16 +1,21 @@
-const http = require('http')
+if (parseInt(process.version.slice(1)) < 8) {
+  console.log('Node Version must greater than v8.0.0 .')
+  process.exit(1)
+}
+
 const program = require('commander');
+const express = require('express');
+const app = express();
 
 program
   .version('0.0.1')
   .option('-p, --port [number]', 'Server listen port, default 8000', '8000')
   .parse(process.argv);
-if (parseInt(process.version.slice(1)) < 8) {
-  console.log('Node Version must greater than v8.0.0 .')
-} else {
-  http.createServer(function (request, response) {
 
-    response.writeHead(200, { 'Content-Type': 'text/plain' });
-    response.end('Hello World\n');
-  }).listen(program.port);
-}
+app.use(express.static('public'));
+
+const serverv = app.listen(program.port, function () {
+  const host = server.address().address;
+  const port = server.address().port;
+  console.log('server running at http://%s:%s', host, port);
+});
